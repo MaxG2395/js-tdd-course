a296fde7944164c478e2f5a7bb5f6c5cf6f39b84
maxg2395 <max_ganzer@gmail.com>
2019-02-15 23:11:32 -0200

    Add check-coverage to prepush hook

--- inicial/tests/main.spec.js
+++ inicial/tests/main.spec.js
@@ -19,4 +19,8 @@ describe('Main', () => {
   it('should return the number when not multiple of 3 or 5', () => {
     expect(FizzBuzz(7)).to.be.equal(7);
   });
+
+  it('should return 0 when 0', () => {
+    expect(FizzBuzz(0)).to.be.equal(0);
+  });
 });
